b9d05cd6bd0d9837453b25ad125d87f25ab3e5fd
cbracquemond <cedricbracquemond@gmail.com>
2022-01-31 20:50:06 +0100

    debugging: login and signup working

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const express = require("express")
-const app = express()
 const mongoose = require("mongoose")
-
+// const cors = require("cors")
 const userRoute = require("./routes/user")
+const app = express()
 
-//permet d'acceder au corps de la requete
-app.use(express.json)
-
-//connection a MongoDB
+// connection a MongoDB
 mongoose.connect('mongodb+srv://cbracquemond:userk421ys@hottakes.lsuue.mongodb.net/HotTakes?retryWrites=true&w=majority',
 { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
-//permet d'éviter les erreurs CORS
-app.use((req, res, next) => {
+// // app.use(cors())
+  
+//   //permet d'éviter les erreurs CORS
+  app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next()
-})
+  })
+  
+// //permet d'acceder au corps de la requete
+app.use(express.json())
+
+// app.use("/", (req, res, next) => {
+//   console.log("hello world")
+//   return res.status(200).json({ "Message": "hello world"})
+// })
 
 app.use("/api/auth", userRoute)
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 module.exports = app

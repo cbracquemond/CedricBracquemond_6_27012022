1e06c0ead3a4622d2a8ec22a5c7d3eb961fae615
cbracquemond <cedricbracquemond@gmail.com>
2022-02-02 12:21:20 +0100

    Multer config and sauce schema writing

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
 const express = require("express")
 const mongoose = require("mongoose")
-// const cors = require("cors")
+const cors = require("cors")
 const userRoute = require("./routes/user")
 const app = express()
+
+//Middlewares:
+//Permet d'éviter les erreurs CORS
+app.use(cors())
+//permet d'acceder au corps de la requete
+app.use(express.json())
 
 // connection a MongoDB
 mongoose.connect('mongodb+srv://cbracquemond:userk421ys@hottakes.lsuue.mongodb.net/HotTakes?retryWrites=true&w=majority',
 { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-// // app.use(cors())
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
   
-//   //permet d'éviter les erreurs CORS
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next()
-  })
-  
-// //permet d'acceder au corps de la requete
-app.use(express.json())
-
-// app.use("/", (req, res, next) => {
-//   console.log("hello world")
-//   return res.status(200).json({ "Message": "hello world"})
-// })
-
-app.use("/api/auth", userRoute)
-
+//Routes
+app.post("/api/auth", userRoute)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

e3a19ebe9e6cb2da9c6282cd6b24874d1bf0a3b6
cbracquemond <cedricbracquemond@gmail.com>
2022-02-08 13:19:30 +0100

    Adding checks to delete, fixing create

--- controlers/sauce.js
+++ controlers/sauce.js
@@ -1,14 +1,23 @@
 const Sauce = require("../models/sauce")
 const fs = require("fs")
+const jwt = require("jsonwebtoken")
 
 exports.createSauce = (req, res) => {
 	const sauceObject = JSON.parse(req.body.sauce)
 	delete sauceObject._id
+	const token = req.headers.authorization.split(" ")[1]
+	const decodedToken = jwt.verify(token, "3}.FKF.xGBxhPT5")
+	const userId = decodedToken.userId
 	const sauce = new Sauce({
-		...sauceObject,
+		userId: userId,
+		name: sauceObject.name,
+		manufacturer: sauceObject.manufacturer,
+		description: sauceObject.description,
+		mainPepper: sauceObject.mainPepper,
 		imageUrl: `${req.protocol}://${req.get("host")}/images/${
 			req.file.filename
 		}`,
+		heat: sauceObject.heat,
 		likes: 0,
 		dislikes: 0,
 		usersLiked: [],
@@ -32,16 +41,6 @@ exports.getSauces = (req, res) => {
 		.catch((error) => res.status(400).json({ error }))
 }
 
-// exports.updateSauce = (req, res) => {
-// 	console.log(req.body)
-// 	if (req.body === null) {
-// 		res.status(400).json({ message: "le body est vide" })
-// 	}
-// 	Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-// 		.then(() => res.status(200).json({ message: "Objet modifié !" }))
-// 		.catch((error) => res.status(400).json({ error }))
-// }
-
 exports.updateSauce = (req, res, next) => {
 	console.log(req.body)
 	const sauceObject = req.file
@@ -63,14 +62,18 @@ exports.updateSauce = (req, res, next) => {
 exports.deleteSauce = (req, res) => {
 	Sauce.findOne({ _id: req.params.id })
 		.then((sauce) => {
-			if (req.body.userIdFromToken !== sauce.userId) {
+			if (req.body.userIdFromToken === sauce.userId) {
+				const filename = sauce.imageUrl.split("/images/")[1]
+				fs.unlink(`images/${filename}`, () => {
+					Sauce.deleteOne({ _id: req.params.id })
+						.then(() => res.status(200).json({ message: "Objet supprimé !" }))
+						.catch((error) => res.status(400).json({ error }))
+				})
+			} else {
+				res
+					.status(400)
+					.json({ message: "Vous n'avez pas les droits pour cette requête!" })
 			}
-			const filename = sauce.imageUrl.split("/images/")[1]
-			fs.unlink(`images/${filename}`, () => {
-				Sauce.deleteOne({ _id: req.params.id })
-					.then(() => res.status(200).json({ message: "Objet supprimé !" }))
-					.catch((error) => res.status(400).json({ error }))
-			})
 		})
 		.catch((error) => res.status(500).json({ error }))
 }

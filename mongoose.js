6e4647e75e8c3ddc65e9dbfe7c0853551f188740
cbracquemond <cedricbracquemond@gmail.com>
2022-02-15 12:39:22 +0100

    More change on mongoose connection setting

--- mongoose.js
+++ mongoose.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose")
 const password = process.env.DB_PASSWORD
 const userName = process.env.DB_USERNAME
 const database = process.env.DB_NAME
+const clusterName = process.env.DB_CLUSTERNAME
 
-const uri = `mongodb+srv://${userName}:${password}@testcluster.lsuue.mongodb.net/${database}?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${userName}:${password}@${clusterName}.lsuue.mongodb.net/${database}?retryWrites=true&w=majority`
 
 mongoose
 	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
